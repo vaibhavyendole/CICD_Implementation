ec323eeadb257e5577f99ab6ae5a301c4a22c38f
vaibhavyendole <vaibhav.yendole@gmail.com>
2026-02-09 11:47:48 +0530

    this is 4th push

--- src/test/java/Day69_/Program2.java
+++ src/test/java/Day69_/Program2.java
@@ -5,6 +5,8 @@ public class Program2 {
 	public static void main(String[] args) {
 		
 		System.out.println("code 2 ");
+		
+		System.out.println("code 2 ");
 		// TODO Auto-generated method stub
 
 	}
