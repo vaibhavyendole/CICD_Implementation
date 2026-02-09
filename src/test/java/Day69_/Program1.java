f11fe30238c9cb03f10df66f99884f5908d2d3bc
vaibhavyendole <vaibhav.yendole@gmail.com>
2026-02-09 10:34:19 +0530

    Comitting for Change 3

---
+++ src/test/java/Day69_/Program1.java
@@ -0,0 +1,16 @@
+package Day69_;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	
+	public void addition()
+	{
+		System.out.println("This is addition");
+	}
+
+}
