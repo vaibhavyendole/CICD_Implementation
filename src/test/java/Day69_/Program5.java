9aaf8e892366380cbeda903948d6af019ac06a99
vaibhavyendole <vaibhav.yendole@gmail.com>
2026-02-12 10:03:11 +0530

    commit for f1

---
+++ src/test/java/Day69_/Program5.java
@@ -0,0 +1,11 @@
+package Day69_;
+
+public class Program5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Vaibhav Write his Code in f1");
+	}
+
+}
